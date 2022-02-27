796220b378f80bc7fb115043c79774ba7ebbebeb
hajnxg <hajnxg@126.com>
2022-02-27 20:47:19 +0800

    fix netty conn oom issue

--- xxl-rpc-core/src/main/java/com/xxl/rpc/remoting/net/impl/netty/client/NettyConnectClient.java
+++ xxl-rpc-core/src/main/java/com/xxl/rpc/remoting/net/impl/netty/client/NettyConnectClient.java
@@ -4,6 +4,8 @@ import com.xxl.rpc.remoting.invoker.XxlRpcInvokerFactory;
 import com.xxl.rpc.remoting.net.common.ConnectClient;
 import com.xxl.rpc.remoting.net.impl.netty.codec.NettyDecoder;
 import com.xxl.rpc.remoting.net.impl.netty.codec.NettyEncoder;
+import com.xxl.rpc.remoting.net.impl.netty_http.client.NettyHttpConnectClient;
+import com.xxl.rpc.remoting.net.params.BaseCallback;
 import com.xxl.rpc.remoting.net.params.Beat;
 import com.xxl.rpc.remoting.net.params.XxlRpcRequest;
 import com.xxl.rpc.remoting.net.params.XxlRpcResponse;
@@ -13,11 +15,11 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
+
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,38 +28,51 @@ import java.util.concurrent.TimeUnit;
  * @author xuxueli
  */
 public class NettyConnectClient extends ConnectClient {
+    private static NioEventLoopGroup nioEventLoopGroup;
 
-
-    private EventLoopGroup group;
     private Channel channel;
 
 
     @Override
     public void init(String address, final Serializer serializer, final XxlRpcInvokerFactory xxlRpcInvokerFactory) throws Exception {
-        final NettyConnectClient thisClient = this;
-
+        // address
         Object[] array = IpUtil.parseIpPort(address);
         String host = (String) array[0];
         int port = (int) array[1];
 
+        // group
+        if (nioEventLoopGroup == null) {
+            synchronized (NettyHttpConnectClient.class) {
+                if (nioEventLoopGroup == null) {
+                    nioEventLoopGroup = new NioEventLoopGroup();
+                    xxlRpcInvokerFactory.addStopCallBack(new BaseCallback() {
+                        @Override
+                        public void run() throws Exception {
+                            nioEventLoopGroup.shutdownGracefully();
+                        }
+                    });
+                }
+            }
+        }
 
-        this.group = new NioEventLoopGroup();
+        // init
+        final NettyConnectClient thisClient = this;
         Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(group)
-                .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel channel) throws Exception {
-                        channel.pipeline()
-                                .addLast(new IdleStateHandler(0,0,Beat.BEAT_INTERVAL, TimeUnit.SECONDS))    // beat N, close if fail
-                                .addLast(new NettyEncoder(XxlRpcRequest.class, serializer))
-                                .addLast(new NettyDecoder(XxlRpcResponse.class, serializer))
-                                .addLast(new NettyClientHandler(xxlRpcInvokerFactory, thisClient));
-                    }
-                })
-                .option(ChannelOption.TCP_NODELAY, true)
-                .option(ChannelOption.SO_KEEPALIVE, true)
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000);
+        bootstrap.group(nioEventLoopGroup)
+            .channel(NioSocketChannel.class)
+            .handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel channel) throws Exception {
+                    channel.pipeline()
+                        .addLast(new IdleStateHandler(0,0,Beat.BEAT_INTERVAL, TimeUnit.SECONDS))    // beat N, close if fail
+                        .addLast(new NettyEncoder(XxlRpcRequest.class, serializer))
+                        .addLast(new NettyDecoder(XxlRpcResponse.class, serializer))
+                        .addLast(new NettyClientHandler(xxlRpcInvokerFactory, thisClient));
+                }
+            })
+            .option(ChannelOption.TCP_NODELAY, true)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000);
         this.channel = bootstrap.connect(host, port).sync().channel();
 
         // valid
@@ -82,9 +97,6 @@ public class NettyConnectClient extends ConnectClient {
     public void close() {
         if (this.channel != null && this.channel.isActive()) {
             this.channel.close();        // if this.channel.isOpen()
-        }
-        if (this.group != null && !this.group.isShutdown()) {
-            this.group.shutdownGracefully();
         }
         logger.debug(">>>>>>>>>>> xxl-rpc netty client close.");
     }
